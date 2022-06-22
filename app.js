f4addc9c4259c14f718dfa0e796ae9340eddcbdf
Kevin <kevinpereaenriquez@gmail.com>
2022-06-22 11:33:48 +0200

    minorchanges

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express =  require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const app = express()
-//const User = require('./models/user.js')
 const port = 3000;
 
 // template engine
 app.set('view engine', 'ejs')
 
-//database to MONGODB
-const dbURI = 'mongodb+srv://saintkevinst:5122012@cluster0.mbhd5.mongodb.net/kev-page?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(port, () => {
-        console.log(`App listening on ${port}`)
-    }))
-    .catch((err) => console.log(err))
-
 //static files and middleware
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+//Listening
+app.listen(port, function(err){
+    if (err) console.log(err);
+    console.log(`server running in ${port}`);
+ });
 
 //rendering pages
 app.get('/', (req, res) => {
